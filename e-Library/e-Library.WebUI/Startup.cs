a9a15d8e5d0c3590d3056ee59e91ced1e6cc851c
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2021-05-11 08:46:53 +0200

    Initial Solution Setup

---
+++ e-Library/e-Library.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_Library.WebUI.Startup))]
+namespace e_Library.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
