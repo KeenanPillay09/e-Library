424556bd3b8a5a365791c59e42a86b6d2c8fbfae
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2021-05-11 11:50:09 +0200

    Book Model and InMemory Book Repository Built

---
+++ e-Library/e-Library.Core/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_Library.Core.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+
+        [DisplayName("Book Name")]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [StringLength(30)]
+        public string Author { get; set; }
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+        public string Genre { get; set; }
+
+        [Range(0,5000)]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+        public Book()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
