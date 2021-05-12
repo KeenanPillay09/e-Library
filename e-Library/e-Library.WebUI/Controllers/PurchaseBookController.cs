6252e95ac8d842a513eb8cec8ac13b346c7e6852
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2021-05-12 16:42:45 +0200

    Added Product Listing

---
+++ e-Library/e-Library.WebUI/Controllers/PurchaseBookController.cs
@@ -0,0 +1,43 @@
+﻿using e_Library.Core.Contracts;
+using e_Library.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Library.WebUI.Controllers
+{
+    public class PurchaseBookController : Controller
+    {
+        IRepository<Book> context;
+        IRepository<BookGenre> bookGenres;
+        IRepository<BookAuthor> bookAuthors;
+
+        public PurchaseBookController(IRepository<Book> bookContext, IRepository<BookGenre> bookGenreContext, IRepository<BookAuthor> bookAuthorContext) //Needs to inject Repositories from DI Container
+        {
+            context = bookContext;
+            bookGenres = bookGenreContext;
+            bookAuthors = bookAuthorContext;
+        }
+        // GET: PurchaseBook
+        public ActionResult Index()
+        {
+            List<Book> books = context.Collection().ToList();
+            return View(books);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Book book = context.Find(Id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(book);
+            }
+        }
+    }
+}
