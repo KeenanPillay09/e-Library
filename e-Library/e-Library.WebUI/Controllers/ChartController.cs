fd99c754e64fc279da1bc985577cafb5030ac69b
KeenanPillay09 <54399643+KeenanPillay09@users.noreply.github.com>
2021-10-05 13:19:24 +0200

    Added Demo Chart

---
+++ e-Library/e-Library.WebUI/Controllers/ChartController.cs
@@ -0,0 +1,39 @@
+﻿using e_Library.Core.Contracts;
+using e_Library.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Library.WebUI.Controllers
+{
+    public class ChartController : Controller
+    {
+        IRepository<Book> books;
+
+        public ChartController(IRepository<Book> booksContext)
+        {
+            books = booksContext;
+        }
+
+
+        public ActionResult ChartOptions()
+        {
+            return View();
+        }
+
+        // GET: Chart
+        public ActionResult Index()
+        {
+            List<Book> book = books.Collection().ToList();
+            return View(book);
+        }
+
+        public ActionResult DisplayChart()
+        {
+            List<Book> book = books.Collection().ToList();
+            return View(book);
+        }
+    }
+}
